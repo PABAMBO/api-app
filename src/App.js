c482d34b56b65af139bb8f5dbf0c2253d83e66b2
Phishego Ashleigh Bambo <maledibamboa@gmail.com>
2024-05-16 12:51:44 +0200

    updated and changed app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [facts, setFacts] = useState([]);
+	const [characters, setCharacters] = useState([]);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-  const getFact = () => {
-    fetch('http://numbersapi.com/random/year?json')
+	const getData = () => {
+    fetch('https://swapi.dev/api/people/')
       .then((res) => res.json())
-      .then((data) => setFacts(data));
+      .then((data) => {
+      	setCharacters(data.results);
+    })
+      .catch((error) => {
+    	return ('Error fecthing data', error);
+    });	
   };
 
-return (
+  const nextCharacter = () => {
+  	setCurrentIndex(currentIndex === characters.length - 1 ? 0 : currentIndex + 1);
+  };
+
+	  return (
     <div>
-      <h1>Click the following button for a random fact about a random year! 
-      <br />
-      Who knows? You might learn something interesting...
-      </h1>
-      <button onClick={getFact}>Click Me!!!</button>
-      <div className='randomFact' key='text'>
-        Your random fact is that: {facts.text}
-      </div>
-      <footer>API requested from numbersapi.com ^_^</footer>
+      <h1>Click below to generate a Star Wars character and their details! Who Knows? You might bump into a favorite of yours...</h1>
+      <button className='first' onClick={getData}>Click Me!!!</button>
+      {characters.length > 0 && (
+      	<div> 
+      	<div className='randomFact'>
+      	<h2>{characters[currentIndex].name}</h2>
+      	<p>Height: {characters[currentIndex].height}</p>
+      	<p>Mass: {characters[currentIndex].mass}</p>
+      	<p>Birthday: {characters[currentIndex].birth_year}</p>
+      	</div>
+      	<button className='second' onClick={nextCharacter}>Click To See Another Character!!!</button>
+      	</div>
+      	)}
     </div>
   );
 };
